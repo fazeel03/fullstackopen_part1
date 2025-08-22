759ded0a6fc0eee37f15c7422a80c05923bf59b8
Fazeel <156305680+fazeel03@users.noreply.github.com>
2025-08-22 09:25:44 +0530

    1.7
    exercise 1.7 completed

--- unicafe/App.jsx
+++ unicafe/App.jsx
@@ -8,6 +8,12 @@ const App = () => {
   const handleNeutral = () => setNeutral(neutral+1);
   const handleBad = () => setBad(bad+1);
 
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1)
+    / total;
+    const positivePercentage = total === 0 ? 0 : (good /total) * 100;
+
+
   return(
     <div>
     <h1>Give Feedback</h1>
@@ -16,9 +22,18 @@ const App = () => {
     <button onClick={handleBad}>Bad</button>
 
     <h2>Statistics</h2>
+    {total === 0 ? (
+      <p>No feedback given</p>
+    ) : (
+    <div>
     <p>Good {good}</p>
     <p>Neutral {neutral}</p>
     <p>Bad {bad}</p>
+    <p>all {total}</p>
+    <p>average {average.toFixed(1)}</p>
+    <p>positive {positivePercentage.toFixed(1)}%</p>
+    </div>
+    )}
     </div>
   );
 };
